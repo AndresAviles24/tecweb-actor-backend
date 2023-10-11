846c75b2c8e73537ab3408ad53b73f60b21a4065
AndresAviles24 <andres.aviles09@gmail.com>
2023-10-10 23:51:34 -0400

    Solicitudes http express

--- controllers/actorsController.js
+++ controllers/actorsController.js
@@ -1,7 +1,6 @@
-// controllers/actorsController.js
-
 const db = require('../db/connection');
 
+// Obtener todos los actores
 exports.getAllActors = async (req, res) => {
     try {
         const [actors] = await db.query('SELECT * FROM actor');
@@ -11,28 +10,48 @@ exports.getAllActors = async (req, res) => {
     }
 };
 
-// Aquí puedes agregar los otros controladores (crear actor, actualizar actor, eliminar actor, etc.)
 // Obtener un actor por ID
 exports.getActorById = async (req, res) => {
     const actorId = req.params.id;
-    // Implementa la lógica aquí...
+    try {
+        const [actor] = await db.query('SELECT * FROM actor WHERE actor_id = ?', [actorId]);
+        res.json(actor[0]);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 // Crear un nuevo actor
 exports.createActor = async (req, res) => {
     const { first_name, last_name } = req.body;
-    // Implementa la lógica aquí...
+    try {
+        const [result] = await db.query('INSERT INTO actor (first_name, last_name) VALUES (?, ?)', [first_name, last_name]);
+        const newActorId = result.insertId;
+        res.status(201).json({ actorId: newActorId });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 // Actualizar actor por ID
 exports.updateActor = async (req, res) => {
     const actorId = req.params.id;
     const { first_name, last_name } = req.body;
-    // Implementa la lógica aquí...
+    try {
+        await db.query('UPDATE actor SET first_name = ?, last_name = ? WHERE actor_id = ?', [first_name, last_name, actorId]);
+        res.status(204).end();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 // Eliminar actor por ID
 exports.deleteActor = async (req, res) => {
     const actorId = req.params.id;
-    // Implementa la lógica aquí...
+    try {
+        await db.query('DELETE FROM actor WHERE actor_id = ?', [actorId]);
+        res.status(204).end();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
